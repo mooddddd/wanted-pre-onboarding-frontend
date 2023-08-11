51d13831245598977a880491086249f36c05557e
mood <mooddddd@naver.com>
2023-08-11 21:14:48 +0900

    feat: todo 로컬 테스트 완료

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -4,17 +4,22 @@ import { useInput } from '../organisms/hooks';
 
 export const Todo = () => {
   const [list, setList] = useState([]);
+  const [modify, setModify] = useState(null);
+  const [newTodo, setNewTodo] = useState('');
+  const [deleteId, setDeleteId] = useState(null);
 
   const todo = useInput('');
   const data = localStorage.getItem('access_token').split('"');
   const token = data[1];
 
+  // 리스트
   const getList = async () => {
     setAccessToken(token);
     const { data } = await request.get('/todos');
     setList(data);
   };
 
+  // 등록
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
@@ -23,20 +28,55 @@ export const Todo = () => {
         todo: todo.value,
       };
       await request.post('/todos', body);
+      getList();
     } catch (e) {
       console.log(e.message);
     }
   };
 
+  // 체크박스 업데이트
   const checkHandler = async (todoId, isCompleted) => {
     try {
       setAccessToken(token);
       const body = {
-        todo: list[todoId - 1].todo,
+        todo: list.find((v) => v.id === todoId).todo,
         isCompleted,
       };
       const { data } = await request.put(`/todos/${todoId}`, body);
-      console.log(data);
+      getList();
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  // 내용 수정
+  const newTodoHandler = (e) => {
+    setNewTodo(e.target.value);
+  };
+  const modifyTodoId = (todoId) => {
+    setModify(todoId);
+  };
+  const modifyHandler = async (todoId, newTodo) => {
+    try {
+      setAccessToken(token);
+      const body = {
+        todo: newTodo,
+        isCompleted: list.find((v) => v.id === todoId).isCompleted,
+      };
+      const { data } = await request.put(`/todos/${todoId}`, body);
+      setModify(null);
+      getList();
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  // 삭제
+  const deleteHandler = async (todoId) => {
+    setDeleteId(todoId);
+    try {
+      setAccessToken(token);
+      await request.delete(`/todos/${todoId}`);
       getList();
     } catch (e) {
       console.log(e.message);
@@ -48,7 +88,6 @@ export const Todo = () => {
       if (!token) return;
       try {
         getList();
-        console.log(list);
       } catch (e) {
         console.log(e.message);
       }
@@ -57,16 +96,62 @@ export const Todo = () => {
 
   const todoMap = list.map((v) => (
     <li key={v.id}>
-      <label>
-        <input
-          type='checkbox'
-          checked={v.isCompleted}
-          onChange={(e) => checkHandler(v.id, e.target.checked)}
-        />
-        <span>{v.todo}</span>
-      </label>
-      <button data-testid='modify-button'>수정</button>
-      <button data-testid='delete-button'>삭제</button>
+      {modify === v.id ? (
+        <form>
+          <label>
+            <input
+              type='checkbox'
+              checked={v.isCompleted}
+              onChange={(e) => checkHandler(v.id, e.target.checked)}
+            />
+            <input
+              data-testid='modify-input'
+              defaultValue={v.todo}
+              onChange={newTodoHandler}
+            />
+          </label>
+          <button
+            data-testid='submit-button'
+            onClick={(e) => {
+              e.preventDefault();
+              modifyHandler(v.id, newTodo);
+            }}
+          >
+            제출
+          </button>
+          <button data-testid='cancel-button' onClick={() => setModify(null)}>
+            취소
+          </button>
+        </form>
+      ) : (
+        <>
+          <label>
+            <input
+              type='checkbox'
+              checked={v.isCompleted}
+              onChange={(e) => checkHandler(v.id, e.target.checked)}
+            />
+            <span>{v.todo}</span>
+          </label>
+          <button
+            data-testid='modify-button'
+            onClick={(e) => {
+              e.preventDefault();
+              modifyTodoId(v.id);
+            }}
+          >
+            수정
+          </button>
+          <button
+            data-testid='delete-button'
+            onClick={() => {
+              deleteHandler(v.id);
+            }}
+          >
+            삭제
+          </button>
+        </>
+      )}
     </li>
   ));
 
@@ -85,18 +170,3 @@ export const Todo = () => {
     </>
   );
 };
-
-// todo list 목록 보이기
-// 내용과 완료 여부 표시 필수
-//
-
-{
-  /* <li>
-<label>
-  <input type='checkbox' />
-  <input data-testid='modify-input' />
-</label>
-<button data-testid='submit-button'>제출</button>
-<button data-testid='cancel-button'>취소</button>
-</li> */
-}
